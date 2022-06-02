db8a245dc0f8a63b0255f5f21a60680a642401a9
izawasoma <izawasoma@gmail.com>
2022-06-02 13:14:00 +0900

    会員登録処理を作成

--- src/signUp.js
+++ src/signUp.js
@@ -5,18 +5,36 @@ import React, { useState } from "react";
 const SignUp = () => {
 
     /* point2 */
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
-    const [decideEmail, setDecideEmail] = useState("");
     const [pass, setPass] = useState("");
-    const [decidePass, setDecidePass] = useState("");
 
     /* ↓表示ボタンをクリックした時の処理 */
     const onClickDisplayValue = () => {
-        const mark = "●";
-        let hiddenPass = mark.repeat(pass.length);
-        let makedEmail = email + "@mail.com";
-        setDecideEmail(hiddenPass);
-        setDecidePass(makedEmail);
+        var url = "https://api-for-missions-and-railways.herokuapp.com/users";
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                "name": name,
+                "email": email,
+                "password": pass
+            }),
+        }).then(response => response.json())
+        .then(responseJson => {
+            console.log(responseJson);
+            let msg;
+            if(responseJson.ErrorCode !== undefined){
+                 msg = responseJson.ErrorMessageJP + "：入力内容を今一度ご確認ください。";
+            }
+            else{
+                 msg = "登録に成功しました。<br>トークン：" + responseJson.token;
+            }
+            let msg_area = document.getElementById("msg");
+            msg_area.textContent = msg;
+        }).catch(error => this.setState({
+                isLoading: false,
+                message: 'Something bad happened ' + error
+        }));
     }
 
     return (
@@ -24,26 +42,28 @@ const SignUp = () => {
             <h1>新規登録ページ</h1>
             <div>
                 {/* point3 */}
-                <p>メールアドレス<input 
-                type="text" 
-                value={email} 
-                onChange={(event) => setEmail(event.target.value)}
+                <p>氏名<input
+                    type="text"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
                 /></p>
-                <p>パスワード<input 
-                type="text" 
-                value={pass} 
-                onChange={(event) => setPass(event.target.value)}
+                <p>メールアドレス<input
+                    type="text"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
                 /></p>
-                <button onClick={onClickDisplayValue}>表示</button>
-                {/* 表示部分 */}
-                <p>パスワード：{decidePass}</p>
-                <p>メールアドレス：{decideEmail}</p>
+                <p>パスワード<input
+                    type="text"
+                    value={pass}
+                    onChange={(event) => setPass(event.target.value)}
+                /></p>
+                <button onClick={onClickDisplayValue}>登録</button>
             </div>
+            <p id="msg"></p>
             <div>
                 <p>ログインは<Link to={`/login/`}>こちら</Link></p>
                 <p><Link to={`/`}>ホームに戻る</Link></p>
             </div>
-            <input type="text"></input>
         </>
     );
 };
